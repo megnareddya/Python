5c611bcaaf72cc35bd53b29bc6c8c6ffd22f1f79
megnareddya <meghanaanantha28@gmail.com>
2024-06-07 11:31:25 +0530

    tuple datatype

---
+++ Exercise_03052024/Lab007.py
@@ -0,0 +1,24 @@
+#tuple data type
+#1.order is preserved
+a=(1,4,2)
+print(a)
+print(type(a))
+#2.duplicate elements are allowed
+b=(10,2,3,4,10,4)
+print(b)
+#3.immutable(cannot change the value)
+#c=(1,7,4)
+#c[2]=5
+#print(C)
+#4.heterogenous
+
+#5.not growable in nature
+
+#6.datatype
+z=(10)
+print(type(z))
+
+y=(10,)
+print(type(y))
+
+
